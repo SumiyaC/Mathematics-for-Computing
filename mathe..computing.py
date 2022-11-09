89935e037ab6532ed27b7244acff130c35c4e4ed
Sumiya Chowdhury <102297880+SumiyaC@users.noreply.github.com>
2022-11-09 19:34:00 +0200

    Add files via upload
    mathe...computing- Write a program that receives two truth values from the user and prints out negation, conjunction, disjunction, and conditional compound propositions.

---
+++ mathe..computing.py
@@ -0,0 +1,7 @@
+x=int(input("Enter 0 or 1: "))
+y=int(input("Enter 0 or 1: "))
+print("conjunction:", bool(x and y))
+print("disjunction:", bool(x or y))
+print("negation:", not x)
+print("conditional compound:", y<=x)
+
